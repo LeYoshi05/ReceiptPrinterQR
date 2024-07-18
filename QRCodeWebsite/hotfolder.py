64834669ea28101ef6121700e83bf86775e5208a
KlSchlitzohr <raspberrypifelix@gmail.com>
2024-07-18 20:11:24 +0200

    Adding HotFolder

---
+++ QRCodeWebsite/hotfolder.py
@@ -0,0 +1,25 @@
+import os
+import time
+import subprocess
+
+while True:
+  # Verzeichnis für Bilder definieren
+  bilder_verzeichnis = "images"
+
+  # Liste aller Dateien im Verzeichnis erhalten
+  dateien = os.listdir(bilder_verzeichnis)
+
+  # Prüfen, ob Bilder im Verzeichnis vorhanden sind
+  if len(dateien) > 0:
+    # Jedes Bild im Verzeichnis drucken und löschen
+    for datei in dateien:
+      if datei.endswith(".jpg") or datei.endswith(".png"):
+        bild_pfad = os.path.join(bilder_verzeichnis, datei)
+        subprocess.run(["lpr", "-P", "EPSON_TM-T20III", bild_pfad])
+        os.remove(bild_pfad)
+        print(f"Bild {datei} gedruckt und gelöscht.")
+
+  # Wartezeit bis zur nächsten Prüfung festlegen (in Sekunden)
+  wartezeit = 1
+  time.sleep(wartezeit)
+  print("Wartezeit abgelaufen.")
