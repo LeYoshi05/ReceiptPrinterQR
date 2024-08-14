8677fb35619be8c9ae5b18e3d3de34429827fe29
KlSchlitzohr <37418805+KlSchlitzohr@users.noreply.github.com>
2024-08-14 20:23:18 +0200

    bastel windows solution

--- QRCodeWebsite/scripts/hotfolder_windows.py
+++ QRCodeWebsite/scripts/hotfolder_windows.py
@@ -1,26 +1,45 @@
 import os
 import time
-from win32 import win32print
-from win32 import win32api
+from PIL import Image
+
+# Verzeichnis für Bilder definieren
+bilder_verzeichnis = "images"
+irfanview_path = "\"C:\\Program Files\\IrfanView\\i_view64.exe\""
+printer_name = "EPSON TM-T20III Receipt"
+max_width, max_height = 203, 203
 
 while True:
-  # Verzeichnis für Bilder definieren
-  bilder_verzeichnis = "images"
+    # Liste aller Dateien im Verzeichnis erhalten
+    dateien = os.listdir(bilder_verzeichnis)
 
-  # Liste aller Dateien im Verzeichnis erhalten
-  dateien = os.listdir(bilder_verzeichnis)
-  win32print.SetDefaultPrinter("Microsoft Print to PDF")
-  # Prüfen, ob Bilder im Verzeichnis vorhanden sind
-  if len(dateien) > 0:
-    # Jedes Bild im Verzeichnis drucken und löschen
-    for datei in dateien:
-      bild_pfad = os.path.join(bilder_verzeichnis, datei)
-      
-      win32api.ShellExecute(0, "print", bild_pfad, None, ".", 0)
-      os.remove(bild_pfad)
-      print(f"Bild {datei} gedruckt und gelöscht.")
+    # Prüfen, ob Bilder im Verzeichnis vorhanden sind
+    if len(dateien) > 0:
+        # Jedes Bild im Verzeichnis drucken und löschen
+        for datei in dateien:
+            bild_pfad = os.path.abspath(os.path.join(bilder_verzeichnis, datei))
 
-  # Wartezeit bis zur nächsten Prüfung festlegen (in Sekunden)
-  wartezeit = 1
-  time.sleep(wartezeit)
-  print("Wartezeit abgelaufen.")
+            # Bildgröße berechnen, um das Seitenverhältnis beizubehalten
+            with Image.open(bild_pfad) as img:
+                width, height = img.size
+                aspect_ratio = width / height
+
+                if width > height:
+                    new_width = min(max_width, width)
+                    new_height = int(new_width / aspect_ratio)
+                else:
+                    new_height = min(max_height, height)
+                    new_width = int(new_height * aspect_ratio)
+
+            # IrfanView-Befehl ausführen
+            command = f'{irfanview_path} {bild_pfad} /resize=({new_width},{new_height}) /print="{printer_name}"'
+            print("DEBUG: ",command)
+            os.system(command)
+
+            # Bilddatei löschen
+            os.remove(bild_pfad)
+            print(f"Bild {datei} gedruckt und gelöscht.")
+
+    # Wartezeit bis zur nächsten Prüfung festlegen (in Sekunden)
+    wartezeit = 1
+    time.sleep(wartezeit)
+    print("Wartezeit abgelaufen.")
